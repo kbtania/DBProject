10e09fe59ca6273e3c6c1fad16d835d862b42426
Tania Kobal <kbtania1@gmail.com>
2021-11-21 20:28:40 +0200

    created basic listbox of teachers

--- interface/test.py
+++ interface/test.py
@@ -7,9 +7,16 @@ win = tk.Tk()
 win.title("Розподіл навантаження")
 win.geometry("700x600+100+30")
 
-btn1 = tk.Button(win, text='Завантажити', command=load_teachers)
-btn1.pack()
-
-win.mainloop()
+# btn1 = tk.Button(win, text='Завантажити', command=load_teachers)
+# btn1.pack()
 teachers = middleware.get_all_teachers()
+teachers_listbox = tk.Listbox(win)
+
+
+for i in range(len(teachers)):
+    teachers_listbox.insert(i, teachers[i])
+
+teachers_listbox.pack()
+win.mainloop()
+
 
