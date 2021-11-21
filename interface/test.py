e2eff1d4bb45d2ed3c8902b8f13c62e4b4a4677a
Tania Kobal <kbtania1@gmail.com>
2021-11-21 22:03:33 +0200

    changed table names at DB

--- interface/test.py
+++ interface/test.py
@@ -10,13 +10,14 @@ win.geometry("700x600+100+30")
 # btn1 = tk.Button(win, text='Завантажити', command=load_teachers)
 # btn1.pack()
 teachers = middleware.get_all_teachers()
-teachers_listbox = tk.Listbox(win)
+teachers_listbox = tk.Listbox(win, width=50, height=20)
 
 
 for i in range(len(teachers)):
     teachers_listbox.insert(i, teachers[i])
 
 teachers_listbox.pack()
+teachers_listbox.place(x=50, y=120)
 win.mainloop()
 
 
