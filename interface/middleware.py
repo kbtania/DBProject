18262adc4ff21463e8c98dafcc21135ffa516203
Tania Kobal <kbtania1@gmail.com>
2021-11-21 22:56:27 +0200

    added listbox for subject1

--- interface/middleware.py
+++ interface/middleware.py
@@ -21,7 +21,7 @@ def get_all_teachers():
             print("З'єднання з SQLite закрито.")
     return all_teachers
 
-def get_all_subjects():
+def get_all_subjects1():
     try:
         sqlite_connection = sqlite3.connect('../backend/data.db')
         cursor = sqlite_connection.cursor()
@@ -42,4 +42,23 @@ def get_all_subjects():
     return all_teachers
 
 
+def get_all_subjects2():
+    try:
+        sqlite_connection = sqlite3.connect('../backend/data.db')
+        cursor = sqlite_connection.cursor()
+        print("Успішно підключено до бази даних SQLite.")
+
+        sqlite_select_query = "SELECT subject FROM semester2"
+        all_teachers = []
+        for row in cursor.execute(sqlite_select_query):
+            all_teachers.append(row[0])
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Помилка при підключенні до SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("З'єднання з SQLite закрито.")
+    return all_teachers
 
--- interface/test.py
+++ interface/test.py
@@ -8,6 +8,8 @@ win.title("Розподіл навантаження")
 win.geometry("700x600+100+30")
 
 teachers = middleware.get_all_teachers()
+subjects1 = middleware.get_all_subjects1()
+subjects2 = middleware.get_all_subjects2()
 
 
 
@@ -23,6 +25,32 @@ teachers_listbox.pack()
 
 teachers_listbox.place(relx=0.1, rely=0.75, anchor='sw')
 
+subjects1_listbox = tk.Listbox(win, width=50, height=20)
+for i in range(len(subjects1)):
+    subjects1_listbox.insert(i, subjects1[i])
+
+subjects1_listbox.pack()
+
+subjects1_listbox.place(relx=0.6, rely=0.75, anchor='sw')
+
+OptionList = [
+"1",
+"2"
+]
+variable = tk.StringVar(win)
+variable.set(OptionList[0])
+
+opt = tk.OptionMenu(win, variable, *OptionList)
+opt.config(width=6)
+
+opt.pack()
+opt.place(relx=0.5, rely=0.15, anchor='sw')
+
+l2 = tk.Label(win, text='Оберіть семестр')
+l2.place(relx=0.5, rely=0.1, anchor='sw')
+
+l3 = tk.Label(win, text='Оберіть предмет')
+l3.place(relx=0.7, rely=0.2, anchor='sw')
 win.mainloop()
 
 
