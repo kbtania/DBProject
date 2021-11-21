8dedfe2eade01ebcb81f44ebd66183a86e000b2a
Tania Kobal <kbtania1@gmail.com>
2021-11-21 22:42:20 +0200

    created function for getting subjects from DB

--- interface/test.py
+++ interface/test.py
@@ -7,17 +7,22 @@ win = tk.Tk()
 win.title("Розподіл навантаження")
 win.geometry("700x600+100+30")
 
-# btn1 = tk.Button(win, text='Завантажити', command=load_teachers)
-# btn1.pack()
 teachers = middleware.get_all_teachers()
+
+
+
+l1 = tk.Label(win, text='Оберіть викладача')
+l1.place(relx=0.2, rely=0.2, anchor='sw')
+
+
 teachers_listbox = tk.Listbox(win, width=50, height=20)
-
-
 for i in range(len(teachers)):
     teachers_listbox.insert(i, teachers[i])
 
 teachers_listbox.pack()
-teachers_listbox.place(x=50, y=120)
+
+teachers_listbox.place(relx=0.1, rely=0.75, anchor='sw')
+
 win.mainloop()
 
 
