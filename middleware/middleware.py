8bcf3b6bba8077f86000687776ab372f160d2f35
Tania Kobal <kbtania1@gmail.com>
2021-11-21 20:06:56 +0200

    created function to get all teachers

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,19 +1,26 @@
 import sqlite3
 
-try:
-    sqlite_connection = sqlite3.connect('../backend/data.db')
-    cursor = sqlite_connection.cursor()
-    print("Успішно підключено до бази даних SQLite.")
 
-    sqlite_select_query = "SELECT teacher_name FROM teachers"
-    cursor.execute(sqlite_select_query)
-    record = cursor.fetchall()[0][0]
-    print(record)
-    cursor.close()
+def get_all_teachers():
+    try:
+        sqlite_connection = sqlite3.connect('../backend/data.db')
+        cursor = sqlite_connection.cursor()
+        print("Успішно підключено до бази даних SQLite.")
 
-except sqlite3.Error as error:
-    print("Помилка при підключенні до SQLite", error)
-finally:
-    if (sqlite_connection):
-        sqlite_connection.close()
-        print("З'єднання з SQLite закрито.")
+        sqlite_select_query = "SELECT teacher_name FROM teachers"
+        all_teachers = []
+        for row in cursor.execute(sqlite_select_query):
+            all_teachers.append(row[0])
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Помилка при підключенні до SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("З'єднання з SQLite закрито.")
+    return all_teachers
+
+
+teachers = get_all_teachers()
+print(teachers)
